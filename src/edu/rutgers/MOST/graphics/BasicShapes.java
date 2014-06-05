ddb94b7f4bcc46b807f24a6e52bb9d5a928442e7
Jim Kelley <kelljim0414@yahoo.com>
2014-06-05 19:47:48 -0400

    Test Java2D graphics

---
+++ src/edu/rutgers/MOST/graphics/BasicShapes.java
@@ -0,0 +1,27 @@
+package edu.rutgers.MOST.presentation;
+
+import javax.swing.JFrame;
+
+public class BasicShapes extends JFrame {
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public BasicShapes() {
+
+        initUI();
+    }
+    
+    private void initUI() {
+        
+        setTitle("Basic Shapes");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        add(new Surface());
+        
+        setSize(350, 250);
+        setLocationRelativeTo(null);        
+    }
+}
