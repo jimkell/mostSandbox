98dd4c73b7f0cd5dbdf61dc79b9f6df422b53d5c
Jim Kelley <kelljim0414@yahoo.com>
2014-06-21 17:47:16 -0400

    Created csv file from the 5 subsystems2peg...
    files. File size reduced from 615 MB to 1 MB.

---
+++ etc/subsystem/README.txt
@@ -0,0 +1,5 @@
+This file was created by reading subsystems2peg from ftp://ftp.theseed.org/subsystems/
+The file was too big to read without splitting it into 5 files using GSplit3. Subsystem and 
+associated reaction names and EC Numbers where available was needed, but the other
+items - example "fig|451757.5.peg.1541" were not necessary and removing entries that were
+the same without this item included reduced the size of the file from 615 MB to 1 MB.
--- src/edu/rutgers/MOST/data/TextFileReader.java
+++ src/edu/rutgers/MOST/data/TextFileReader.java
@@ -4,13 +4,19 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 
+import javax.swing.JOptionPane;
+
+import java.io.FileWriter;
+
 public class TextFileReader {
 	
 	ArrayList<String> subsystems = new ArrayList<String>();
+	ArrayList<String> entries = new ArrayList<String>();
 	
 	public void readFile(File file, Character splitChar) {
 		
@@ -24,23 +30,29 @@ public class TextFileReader {
 				temp = br.readLine();
 				//System.out.println(temp);
 				try {
-					String[] s = temp.split("\t");
-					for (int i = 0; i < s.length; i++) {
-						//System.out.println(s[i]);
-						if (i == 0 && !subsystems.contains(s[i])) {
-							subsystems.add(s[i]);
-							//System.out.println(s[i]);
-						} 
-						if (i == 1) {
-							//System.out.println(s[i]);
-							if (s[i].contains("(EC ")) {
-								
-								String ec = s[i].substring(s[i].indexOf("(EC ") + 4, s[i].indexOf(")"));
-								System.out.println(ec);
-							}
-						}
-						//System.out.println(s[i]);
+					String item = temp.substring(0, temp.indexOf("fig"));
+					//System.out.println(item);
+					if (!entries.contains(item)) {
+						entries.add(item);
+						System.out.println(item);
 					}
+//					String[] s = temp.split("\t");
+//					for (int i = 0; i < s.length; i++) {
+//						//System.out.println(s[i]);
+//						if (i == 0 && !subsystems.contains(s[i])) {
+//							subsystems.add(s[i]);
+//							//System.out.println(s[i]);
+//						} 
+//						if (i == 1) {
+//							//System.out.println(s[i]);
+//							if (s[i].contains("(EC ")) {
+//								
+//								String ec = s[i].substring(s[i].indexOf("(EC ") + 4, s[i].indexOf(")"));
+//								System.out.println(ec);
+//							}
+//						}
+//						//System.out.println(s[i]);
+//					}
 				} catch (Exception e) {
 					
 				}
@@ -58,14 +70,32 @@ public class TextFileReader {
 		}	
 	}
 	
+	public void writeFile(String file) {
+		try {
+			FileWriter writer = new FileWriter(file); 
+			for(int i = 0; i < entries.size(); i++) {
+				writer.write(entries.get(i));
+				writer.write("\n");
+			}
+			writer.close();
+		} catch (IOException e) {
+			JOptionPane.showMessageDialog(null,                
+					"File Not Found Error.",                
+					"Error",                                
+					JOptionPane.ERROR_MESSAGE);
+			//e.printStackTrace();
+		}
+	}
+	
 	public static void main(String[] args) {
 		TextFileReader r = new TextFileReader();
-		File f = new File("C:/subsystems2peg1.txt");
-		r.readFile(f, '\t');
-		//for (int i = 1; i <= 5; i++ ) {
-//			File f = new File("C:/subsystems2peg" + i + ".txt");
-//			r.readFile(f, '\t');
-		//}
+//		File f = new File("C:/subsystems2peg1.txt");
+//		r.readFile(f, '\t');
+		for (int i = 1; i <= 5; i++ ) {
+			File f = new File("C:/subsystems2peg" + i + ".txt");
+			r.readFile(f, '\t');
+		}
 		//System.out.println();
+		r.writeFile("subsystems2peg.txt");
 	}
 }
