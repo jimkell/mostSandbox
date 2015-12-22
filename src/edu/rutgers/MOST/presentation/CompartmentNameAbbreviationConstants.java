0ee7ac44984c0b5078b9615e8c9b20173a93edf6
jimkell <kelljim0414@yahoo.com>
2015-12-22 18:45:59 -0500

    Added metabolites, reactions. Removed label...
    from Compartment Name Dialog. Pulled out Calvin Cycle.

--- src/edu/rutgers/MOST/presentation/CompartmentNameAbbreviationConstants.java
+++ src/edu/rutgers/MOST/presentation/CompartmentNameAbbreviationConstants.java
@@ -3,6 +3,7 @@ package edu.rutgers.MOST.presentation;
 public class CompartmentNameAbbreviationConstants {
 
 	public static final String DIALOG_TITLE = "Compartment Name Selection";
-	public static final String TOP_LABEL = "Compartment                                                           Name                                      ";
+	public static final String TOP_LABEL = "";
+//	public static final String TOP_LABEL = "Compartment                                                           Name                                      ";
 	
 }
